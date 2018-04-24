aaf5bd29e970fff2fe4628b111784f04925ee368
Guo Hui <sunnycase@live.cn>
2018-04-24 14:21:18 +0800

    Make cmake works

---
+++ src/bootloader/bootloader.c
@@ -0,0 +1,12 @@
+//
+// Bootloader
+//
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+	InitializeLib(ImageHandle, SystemTable);
+	Print(L"Hello, world!\n");
+	return EFI_SUCCESS;
+}
