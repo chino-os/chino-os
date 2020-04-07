9d52b2a638ba241f8b4a54e4f696b0a15ef2f957
Guo Hui <sunnycase@live.cn>
2020-04-08 00:32:54 +0800

    Update

--- cmake/boards/armtest.cmake
+++ cmake/boards/armtest.cmake
@@ -1,4 +1,5 @@
-enable_language(ASM_MASM)
 set(CHINO_ARCH arm/armv7-m)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mthumb -specs=nosys.specs -march=armv7-m -mcpu=cortex-m3")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mthumb -specs=nosys.specs -march=armv7-m -mcpu=cortex-m3")
 
 set(CHINO_KERNEL_STACK_SIZE "8192")
