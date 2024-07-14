fe3fe94bc27d4fb768fd2e565e4744553c82b4d0
sunnycase <sunnycase@live.cn>
2024-07-14 21:46:56 +0800

    Add shell

--- src/hal/src/arch/arm/armv7-m/arch.cmake
+++
@@ -1,4 +0,0 @@
-set(CHINO_ARCH arm/armv7-m)
-set(COMP_LINK_OPTIONS -mthumb -march=armv7-m -mfloat-abi=soft -mcpu=cortex-m3 -nostartfiles)
-add_compile_options(${COMP_LINK_OPTIONS})
-add_link_options(${COMP_LINK_OPTIONS})
--- src/os/boot/emulator/emulator.cpp
+++ src/os/boot/emulator/emulator.cpp
@@ -74,8 +74,8 @@ void print(std::string_view str) {
 
 } // namespace
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
-                   _In_ int nShowCmd) {
+extern "C" int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
+                              _In_ int nShowCmd) {
     bool show_version = false;
     size_t memory_size = default_memory_size;
 
