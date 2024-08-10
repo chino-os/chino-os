2ba1ce54e4011062f74e333a1413d776fa463ff3
sunnycase <sunnycase@live.cn>
2024-08-10 18:44:41 +0800

    Fix

--- src/os/hal/chips/emulator/cmake/chip.cmake
+++ src/os/hal/chips/emulator/cmake/chip.cmake
@@ -8,7 +8,11 @@ if (NOT DEFINED ENV{WindowsSdkDir})
     message(FATAL_ERROR "Enviroment WindowsSdkDir is not defined")
 endif()
 
-set(WIN_SDK_INCLUDE_ROOT $ENV{WindowsSdkDir}Include\\$ENV{WindowsSdkVersion})
-set(WIN_SDK_INCLUDE_DIR ${WIN_SDK_INCLUDE_ROOT}shared
-                        ${WIN_SDK_INCLUDE_ROOT}um
-                        ${WIN_SDK_INCLUDE_ROOT}winrt)
+if (NOT DEFINED WIN_SDK_INCLUDE_ROOT)
+    set(WIN_SDK_INCLUDE_ROOT $ENV{WindowsSdkDir}Include/$ENV{WindowsSdkVersion})
+    message(STATUS "Fvck")
+endif()
+
+set(WIN_SDK_INCLUDE_DIR ${WIN_SDK_INCLUDE_ROOT}/shared
+                        ${WIN_SDK_INCLUDE_ROOT}/um
+                        ${WIN_SDK_INCLUDE_ROOT}/winrt)
