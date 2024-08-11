b29ecbcca36a02b6a61072c00d69f92587c286f1
sunnycase <sunnycase@live.cn>
2024-08-11 13:31:35 +0800

    Add prepare

---
+++ conf/boards/wch/ch582/prepare.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DOWNLOAD_DIR=$SCRIPT_DIR/../../../../download
+OPENOCD_ARCHIVE=$DOWNLOAD_DIR/wch/openocd_osx-arm64.tar.zst
+GCC_ARCHIVE=$DOWNLOAD_DIR/wch/xpack-riscv-none-elf-gcc-12.2.0-3_osx-arm64.tar.zst
+mkdir $DOWNLOAD_DIR/wch &> /dev/null
+
+# OpenOCD
+if [ ! -f "$OPENOCD_ARCHIVE" ]; then
+  wget https://dav.sunnycase.moe/d/ci/chino/$OPENOCD_ARCHIVE -O $OPENOCD_ARCHIVE
+  tar --zstd -xf $OPENOCD_ARCHIVE -C $DOWNLOAD_DIR
+fi
+
+# GCC
+if [ ! -f "$GCC_ARCHIVE" ]; then
+  wget https://dav.sunnycase.moe/d/ci/chino/$GCC_ARCHIVE -O $GCC_ARCHIVE
+  tar --zstd -xf $GCC_ARCHIVE -C $DOWNLOAD_DIR
+fi
